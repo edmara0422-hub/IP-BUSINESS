07335acb205ea6b74060f9ff139c4f7851e2437e
Edmara <edmara@ipbusiness.app>
2026-03-20 19:48:26 -0300

    s

---
+++ business/capacitor.config.ts
@@ -0,0 +1,19 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.ipbusiness.app',
+  appName: 'IPB',
+  webDir: 'out',
+  backgroundColor: '#060608',
+  ios: {
+    contentInset: 'automatic',
+    preferredContentMode: 'mobile',
+    backgroundColor: '#060608',
+  },
+  server: {
+    androidScheme: 'https',
+    iosScheme: 'https',
+  },
+}
+
+export default config
